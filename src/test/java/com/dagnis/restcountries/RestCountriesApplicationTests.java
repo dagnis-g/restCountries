02c730a313568bf9c9b2db50921dcbaa0c569b0e
DagnisGulbis <dagnisgulbis1@gmail.com>
2022-06-28 15:34:22 +0300

    Added test for Wildcard transform

--- src/test/java/com/dagnis/restcountries/RestCountriesApplicationTests.java
+++ src/test/java/com/dagnis/restcountries/RestCountriesApplicationTests.java
@@ -1,14 +1,26 @@
 package com.dagnis.restcountries;
 
+import com.dagnis.restcountries.service.WildcardTransformerService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestCountriesApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    WildcardTransformerService wildcardTransformerService;
 
+    @Test
+    void transformShouldAddPointBeforeStarAndLowercaseIt() {
+
+        String userEnteredWildcard = "s*N";
+        String transformedWildcard = wildcardTransformerService.transform(userEnteredWildcard);
+        String expectedWildcard = "s.*n";
+
+        Assertions.assertEquals(expectedWildcard, transformedWildcard);
     }
+
 
 }
